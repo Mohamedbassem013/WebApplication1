82dbbd1f66970cbc6491472443bf31c6ff87eb39
Muhammed Bassem <Muhammed Bassem@DESKTOP-DA9SS6B>
2024-10-01 17:00:14 +0300

    Add project files.

---
+++ ClassLibrary1/data/mmm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.data
+{
+    internal class mmm
+    {
+        public int id { get; set; }
+        public int name { get; set; }
+    }
+}
